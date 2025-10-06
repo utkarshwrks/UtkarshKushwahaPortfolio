bc314b342c46f505e55c65480ec7c2b1fe9c9fa5
Utkarsh Kushwaha <utkarshkushwaha246@gmail.com>
2025-10-06 23:22:55 +0530

    sitemapbuild

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,17 +1,7 @@
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://utkarsh-kushwaha.vercel.app',
-  generateRobotsTxt: true,
-  generateIndexSitemap: false,
+  siteUrl: 'https://utkarsh-kushwaha.vercel.app', // ✅ replace with your live site URL
+  generateRobotsTxt: true, // (optional) generate robots.txt
   sitemapSize: 7000,
-
-  // explicitly provide all app routes here
-additionalPaths: async (config) => [
-  await config.transform(config, '/'),
-  await config.transform(config, '/about'),
-  await config.transform(config, '/projects'),
-  await config.transform(config, '/contact'),
-  await config.transform(config, '/experience'),
-]
-
-}
+};
